10c5907ffe08a5b678a2cb4a28d35aa88578c63f
Oleksii Smiichuk <smiychuk@gmail.com>
2023-06-11 07:44:26 +0000

    Changed auth method

---
+++ provider.tf
@@ -0,0 +1,14 @@
+
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.68.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.GOOGLE_PROJECT
+  region = var.GOOGLE_REGION
+}
